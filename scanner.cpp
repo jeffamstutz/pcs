62c8892933e7d3e2125f72689b74ce0707b496fe
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2018-06-04 22:15:04 -0700

    a start on tokenizing lines from a file

--- scanner.cpp
+++ scanner.cpp
@@ -31,6 +31,163 @@
 
 namespace pcs {
 
+  // Helper types /////////////////////////////////////////////////////////////
+
+  enum class TokenKind
+  {
+    IDENTIFIER,
+    OPERATOR,
+    BRACE,
+    SEMICOLON,
+    STRING,
+    NUMBER,
+    WHITESPACE,
+    UNDEFINED
+  };
+
+  // Tokenization helper functions ////////////////////////////////////////////
+
+  static bool isBrace(char c)
+  {
+    return c == '{' || c == '}' || c == '(' || c == ')' || c == '[' || c == ']';
+  }
+
+  static bool isQuote(char c)
+  {
+    return c == '"';
+  }
+
+  static bool isSemicolon(char c)
+  {
+    return c == ';';
+  }
+
+  static bool isWhitespace(char c)
+  {
+    return c == ' ' || c == '\t' || c == '\n';
+  }
+
+  static bool isOperator(char c)
+  {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '<' ||
+           c == '>' || c == '=' || c == '!' || c == '|' || c == '&';
+  }
+
+  static bool isNumber(char c)
+  {
+    return c == '1' || c == '2' || c == '3' || c == '4' || c == '5' ||
+           c == '6' || c == '7' || c == '8' || c == '9' || c == '0';
+  }
+
+  template <TokenKind CURRENT_STATE>
+  TokenKind advance(char nextCharacter, std::string &currentToken)
+  {
+    throw std::runtime_error("unkown version of advance<>() function called!");
+  }
+
+  template <>
+  TokenKind advance<TokenKind::WHITESPACE>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    const auto &c = nextCharacter;
+
+    if (isWhitespace(c)) return TokenKind::WHITESPACE;
+
+    currentToken.push_back(c);
+
+    if (isQuote(c)) return TokenKind::STRING;
+    if (isSemicolon(c)) return TokenKind::SEMICOLON;
+    if (isOperator(c)) return TokenKind::OPERATOR;
+    if (isNumber(c)) return TokenKind::NUMBER;
+    if (isBrace(c)) return TokenKind::BRACE;
+
+    return TokenKind::IDENTIFIER;
+  }
+
+  template <>
+  TokenKind advance<TokenKind::STRING>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    const auto &c = nextCharacter;
+    currentToken.push_back(c);
+    if (isQuote(c)) return TokenKind::WHITESPACE;
+    return TokenKind::STRING;
+  }
+
+  template <>
+  TokenKind advance<TokenKind::IDENTIFIER>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    const auto &c = nextCharacter;
+
+    if (isWhitespace(c)) return TokenKind::WHITESPACE;
+
+    currentToken.push_back(c);
+
+    if (isQuote(c)) return TokenKind::STRING;
+    if (isOperator(c)) return TokenKind::OPERATOR;
+    if (isSemicolon(c)) return TokenKind::SEMICOLON;
+    if (isBrace(c)) return TokenKind::BRACE;
+
+    return TokenKind::IDENTIFIER;
+  }
+
+  template <>
+  TokenKind advance<TokenKind::SEMICOLON>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    return advance<TokenKind::WHITESPACE>(nextCharacter, currentToken);
+  }
+
+  template <>
+  TokenKind advance<TokenKind::OPERATOR>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    return advance<TokenKind::IDENTIFIER>(nextCharacter, currentToken);
+  }
+
+  template <>
+  TokenKind advance<TokenKind::BRACE>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    return advance<TokenKind::IDENTIFIER>(nextCharacter, currentToken);
+  }
+
+  template <>
+  TokenKind advance<TokenKind::NUMBER>(
+    char nextCharacter,
+    std::string &currentToken
+  )
+  {
+    const auto &c = nextCharacter;
+
+    if (isWhitespace(c)) return TokenKind::WHITESPACE;
+
+    currentToken.push_back(c);
+
+    if (isQuote(c)) return TokenKind::STRING;
+    if (isOperator(c)) return TokenKind::OPERATOR;
+    if (isSemicolon(c)) return TokenKind::SEMICOLON;
+    if (isBrace(c)) return TokenKind::BRACE;
+    if (isNumber(c)) return TokenKind::NUMBER;
+
+    return TokenKind::IDENTIFIER;
+  }
+
+  // Misc. helper functions ///////////////////////////////////////////////////
+
   static std::vector<std::string> getLinesFromFile(const std::string &file)
   {
     std::vector<std::string> lines;
@@ -66,12 +223,45 @@ namespace pcs {
     std::vector<std::string> tokens;
 
     std::string token;
+    TokenKind currentKind = TokenKind::WHITESPACE;
 
     for (const auto &l : lines) {
-      auto nonWhitespaceItems = split(l, "\n\t ");
+      for (const auto &c : l) {
+        auto previousKind = currentKind;
+        switch(currentKind) {
+        case TokenKind::WHITESPACE:
+          currentKind = advance<TokenKind::WHITESPACE>(c, token);
+          break;
+        case TokenKind::IDENTIFIER:
+          currentKind = advance<TokenKind::IDENTIFIER>(c, token);
+          break;
+        case TokenKind::OPERATOR:
+          currentKind = advance<TokenKind::OPERATOR>(c, token);
+          break;
+        case TokenKind::BRACE:
+          currentKind = advance<TokenKind::BRACE>(c, token);
+          break;
+        case TokenKind::SEMICOLON:
+          currentKind = advance<TokenKind::SEMICOLON>(c, token);
+          break;
+        case TokenKind::STRING:
+          currentKind = advance<TokenKind::STRING>(c, token);
+          break;
+        case TokenKind::NUMBER:
+          currentKind = advance<TokenKind::NUMBER>(c, token);
+          break;
+        default:
+          currentKind = advance<TokenKind::UNDEFINED>(c, token);
+          break;
+        }
 
-      for (const auto &item : nonWhitespaceItems) {
-        std::cout << "found item: " << item << std::endl;
+        if (previousKind != currentKind &&
+            previousKind != TokenKind::WHITESPACE &&
+            currentKind != TokenKind::STRING &&
+            !token.empty()) {
+          tokens.push_back(token);
+          token.clear();
+        }
       }
     }
 
@@ -84,15 +274,17 @@ namespace pcs {
       return;
 
     for (const auto &token : tokens)
-      std::cout << " | " << token;
-
-    std::cout << " |" << std::endl;
+      std::cout << "token: " << token << std::endl;
   }
+
+  /////////////////////////////////////////////////////////////////////////////
 
   void scanFile(const std::string &file)
   {
     auto lines = getLinesFromFile(file);
     printLines(lines);
+
+    std::cout << std::endl;
 
     auto tokens = tokenize(lines);
     printTokens(tokens);
